aa86d5401928ff9e31015ccba946c460a08ef53e
stabmeldys <lukas.les93@gmail.com>
2024-07-19 17:09:04 +0300

    search

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod inputs;
+mod search;
 
 use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
 
 use inputs::parse::parse_args;
 use inputs::config::Config;
@@ -8,7 +12,23 @@ use inputs::config::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let cfg = parse_args(args).expect("Failed configuration!");
+    let cfg: Config = parse_args(args).expect("Failed configuration!");
+    if let Err(e) = run(cfg){
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
 
-    dbg!(cfg);
+
+fn run(cfg: Config) -> Result<(), Box<dyn Error>> {
+    let target_files: Vec<String> = Vec::new();
+    for target_file in target_files {
+        let content = match fs::read_to_string(target_file) {
+            Ok(f) => f,
+            Err(e) => { 
+                eprintln!("{}", e);
+                continue; },
+        };
+    }
+    Ok(())
 }
---
+++ src/search.rs
@@ -0,0 +1,22 @@
+use regex::Regex;
+
+fn search(content: &String, re_pattern: &String) -> Vec<String> {
+    let re = Regex::new(re_pattern).unwrap();
+    re.find_iter(content)
+        .map(|mat| mat.as_str().to_string())
+        .collect()
+    
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_search() {
+        let content = "qwerty WVWZZZ3CZCE024541 pow".to_string();
+        let re_pattern = "(?i)\\b[A-HJ-NPR-Z0-9]{17}\\b".to_string();
+        assert_eq!(vec!["WVWZZZ3CZCE024541"], search(&content, &re_pattern));
+    }
+}
