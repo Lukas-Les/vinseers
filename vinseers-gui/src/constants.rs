ea131d49598a19becc9d3bcd918f7560af277a8f
stabmeldys <lukas.les93@gmail.com>
2024-09-05 19:35:28 +0300

    extended files ext arr

--- vinseers-gui/src/constants.rs
+++ vinseers-gui/src/constants.rs
@@ -1,4 +1,4 @@
-pub const ALLOWED_FILES: [&str; 5] = ["txt", "html", "json", "csv", "pdf"];
+pub const ALLOWED_FILES: [&str; 8] = ["txt", "html", "json", "csv", "pdf", "docx", "xlsx", "pptx"];
 
 pub const RESULT_TEXT_DEFAULT: &str = "The results will be desplayed here ^^";
 pub const VIN_RE_DEFAULT: &str = "(?i)\\b[A-HJ-NPR-Z0-9]{17}\\b";
