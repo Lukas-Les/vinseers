b1c27aa29bdff28c3366b3ec61b20629f9a0f591
Lukas Leščinskas <71329803+Lukas-Les@users.noreply.github.com>
2024-09-18 11:00:03 +0300

    expanded supported files list

--- vinseers-gui/src/constants.rs
+++ vinseers-gui/src/constants.rs
@@ -1,4 +1,31 @@
-pub const ALLOWED_FILES: [&str; 8] = ["txt", "html", "json", "csv", "pdf", "docx", "xlsx", "pptx"];
+pub const ALLOWED_FILES: [&str; 26] = [
+    "csv",   // CSV (Comma-separated values)
+    "docx",  // Microsoft Word (Open XML)
+    "html",  // HTML
+    "js",    // JavaScript files
+    "json",  // JSON format
+    "jsonl", // JSON Lines
+    "log",   // Log files
+    "md",    // Markdown files
+    "odt",   // OpenDocument Text
+    "ods",   // OpenDocument Spreadsheet
+    "odp",   // OpenDocument Presentation
+    "pdf",   // PDF files
+    "php",   // PHP scripts
+    "pl",    // Perl scripts
+    "pptx",  // Microsoft PowerPoint (Open XML)
+    "py",    // Python scripts
+    "rb",    // Ruby scripts
+    "rs",    // Rust source files
+    "rtf",   // Rich Text Format
+    "sh",    // Shell scripts
+    "sql",   // SQL scripts
+    "ts",    // TypeScript files
+    "txt",   // Plain text
+    "xml",   // XML files
+    "yml",    // YAML files
+    "yaml",
+];
 
 pub const RESULT_TEXT_DEFAULT: &str = "The results will be desplayed here ^^";
 pub const VIN_RE_DEFAULT: &str = "(?i)\\b[A-HJ-NPR-Z0-9]{17}\\b";
