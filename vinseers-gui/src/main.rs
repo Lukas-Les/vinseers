2c77ef5e34160edf36b3546e362237028ad86f95
stabmeldys <lukas.les93@gmail.com>
2024-09-24 19:28:59 +0300

    upgrade to iced 0.13.1

--- vinseers-gui/src/main.rs
+++ vinseers-gui/src/main.rs
@@ -6,72 +6,78 @@ use std::string::ToString;
 use rfd::FileDialog;
 
 use iced::widget::text_editor::Content;
-use iced::widget::{text_editor, Button, Column, Container, Row};
-use iced::{Element, Sandbox, Settings};
+use iced::widget::{row, text_editor, Button, column};
+use iced::{Element, Theme};
 
 pub fn main() -> iced::Result {
-    Vinseers::run(Settings::default())
+    iced::application("vinseers", update, view)
+        .theme(theme)
+        .run()
 }
 
-struct Vinseers {
+fn theme(state: &State) -> Theme {
+    Theme::Dark
+}
+
+
+struct State {
     content: text_editor::Content,
     vin_re: String,
 }
 
-impl Sandbox for Vinseers {
-    type Message = Message;
-
-    fn new() -> Vinseers {
-        Vinseers {
+impl Default for State {
+    fn default() -> Self {
+        Self {
             content: text_editor::Content::with_text(constants::RESULT_TEXT_DEFAULT),
             vin_re: constants::VIN_RE_DEFAULT.to_string(),
         }
     }
+}
 
-    fn title(&self) -> String {
-        String::from("vinseers")
-    }
-
-    fn update(&mut self, message: Self::Message) {
-        match message {
-            Message::SelectFiles => {
-                let target_paths = FileDialog::new()
-                    .add_filter("text", &constants::ALLOWED_FILES)
-                    .pick_files();
-                if let Some(paths) = target_paths {
-                    let result = helpers::process_paths(&paths, &self.vin_re);
-                    self.content = Content::with_text(result.join("\n").as_str());
-                }
-            }
-            Message::SelectDir => {
-                let target_paths = FileDialog::new().pick_folders();
-                if let Some(paths) = target_paths {
-                    let result = helpers::process_paths(&paths, &self.vin_re);
-                    self.content = Content::with_text(result.join("\n").as_str());
-                }
-            }
-            Message::ResetResult => {
-                self.content = text_editor::Content::with_text(constants::RESULT_TEXT_DEFAULT);
-            }
-            Message::AnyActionPerformed(action) => {
-                self.content.perform(action);
+fn update(state: &mut State, message: Message) {
+    match message {
+        Message::SelectFiles => {
+            let target_paths = FileDialog::new()
+                .add_filter("text", &constants::ALLOWED_FILES)
+                .pick_files();
+            if let Some(paths) = target_paths {
+                let result = helpers::process_paths(&paths, &state.vin_re);
+                state.content = Content::with_text(result.join("\n").as_str());
             }
         }
-    }
-
-    fn view(&self) -> Element<Self::Message> {
-        let top_row = Row::new()
-            .push(Button::new("Select Files").on_press(Message::SelectFiles))
-            .push(Button::new("Select directory").on_press(Message::SelectDir))
-            .push(Button::new("Reset").on_press(Message::ResetResult))
-            .spacing(3);
-
-        let top_row_container = Container::new(top_row).padding(3).center_x();
-        let result_display = text_editor(&self.content).on_action(Message::AnyActionPerformed);
-        let content = Column::new().push(top_row_container).push(result_display);
-        content.into()
+        Message::SelectDir => {
+            let target_paths = FileDialog::new().pick_folders();
+            if let Some(paths) = target_paths {
+                let result = helpers::process_paths(&paths, &state.vin_re);
+                state.content = Content::with_text(result.join("\n").as_str());
+            }
+        }
+        Message::ResetResult => {
+            state.content = text_editor::Content::with_text(constants::RESULT_TEXT_DEFAULT);
+        }
+        Message::AnyActionPerformed(action) => {
+            state.content.perform(action);
+        }
     }
 }
+
+fn view(state: &State) -> Element<Message> {
+    let top_row = row![
+        Button::new("Select Files").on_press(Message::SelectFiles),
+        Button::new("Select directory").on_press(Message::SelectDir),
+        Button::new("Reset").on_press(Message::ResetResult),
+    ]
+        .spacing(3);
+    let result_display = text_editor(&state.content).on_action(Message::AnyActionPerformed);
+
+    column![
+        top_row,
+        result_display,
+    ]
+        .spacing(3)
+        .into()
+}
+
 
 #[derive(Clone, Debug)]
 enum Message {
