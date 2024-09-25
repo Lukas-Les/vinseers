aa714166e0e9594776faf4618d5443eeac079007
stabmeldys <lukas.les93@gmail.com>
2024-09-25 17:13:54 +0300

    pick list implemented, small fix needed

--- vinseers-gui/src/main.rs
+++ vinseers-gui/src/main.rs
@@ -6,7 +6,7 @@ use std::string::ToString;
 use rfd::FileDialog;
 
 use iced::widget::text_editor::Content;
-use iced::widget::{row, text_editor, Button, column};
+use iced::widget::{row, text_editor, Button, column, pick_list};
 use iced::{Element, Theme};
 
 pub fn main() -> iced::Result {
@@ -22,14 +22,14 @@ fn theme(state: &State) -> Theme {
 
 struct State {
     content: text_editor::Content,
-    current_regex: String,
+    vid: VidType,
 }
 
 impl Default for State {
     fn default() -> Self {
         Self {
             content: text_editor::Content::with_text(constants::RESULT_TEXT_DEFAULT),
-            current_regex: constants::VIN_RE_DEFAULT.to_string(),
+            vid: VidType::Vin,
         }
     }
 }
@@ -41,14 +41,14 @@ fn update(state: &mut State, message: Message) {
                 .add_filter("text", &constants::ALLOWED_FILES)
                 .pick_files();
             if let Some(paths) = target_paths {
-                let result = helpers::process_paths(&paths, &state.current_regex);
+                let result = helpers::process_paths(&paths, &state.vid.to_regex());
                 state.content = Content::with_text(result.join("\n").as_str());
             }
         }
         Message::SelectDir => {
             let target_paths = FileDialog::new().pick_folders();
             if let Some(paths) = target_paths {
-                let result = helpers::process_paths(&paths, &state.current_regex);
+                let result = helpers::process_paths(&paths, &state.vid.to_regex());
                 state.content = Content::with_text(result.join("\n").as_str());
             }
         }
@@ -57,15 +57,30 @@ fn update(state: &mut State, message: Message) {
         }
         Message::AnyActionPerformed(action) => {
             state.content.perform(action);
-        }
+        },
+        Message::VidSelected(vid) => {
+            println!("vid regex: {}", &state.vid.to_regex());
+            match vid {
+                VidType::Vin => state.vid = VidType::Vin,
+                VidType::Lpn(lpn) => {
+                    match lpn {
+                        LpnType::Ltu => state.vid = VidType::Lpn(LpnType::Ltu),
+                    }
+                }
+            }
+        },
     }
 }
 
 fn view(state: &State) -> Element<Message> {
+    let all_vid = [VidType::Vin, VidType::Lpn(LpnType::Ltu)];
+    let re_pick_list = pick_list(all_vid, Some(&state.vid), Message::VidSelected);
+
     let top_row = row![
         Button::new("Select Files").on_press(Message::SelectFiles),
         Button::new("Select directory").on_press(Message::SelectDir),
         Button::new("Reset").on_press(Message::ResetResult),
+        re_pick_list,
     ]
         .spacing(3);
     let result_display = text_editor(&state.content).on_action(Message::AnyActionPerformed);
@@ -84,6 +99,7 @@ enum Message {
     SelectFiles,
     SelectDir,
     ResetResult,
+    VidSelected(VidType),
     AnyActionPerformed(text_editor::Action),
 }
 
@@ -107,6 +123,19 @@ impl VidType {
             Self::Lpn(t) => {
                 match t {
                     LpnType::Ltu => constants::LPN_LTU_RE.to_string(),
+                }
+            }
+        }
+    }
+}
+
+impl ToString for VidType {
+    fn to_string(&self) -> String {
+        match self {
+            VidType::Vin => "VIN".to_string(),
+            VidType::Lpn(t) => {
+                match t {
+                    LpnType::Ltu => "LPN-LTU".to_string(),
                 }
             }
         }
