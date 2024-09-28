ca768146ba8eac628fff63664e1da3d6a498b9ea
stabmeldys <lukas.les93@gmail.com>
2024-09-28 08:12:47 +0300

    added LPN's regexes

--- vinseers/src/regexes.rs
+++ vinseers/src/regexes.rs
@@ -1,2 +1,6 @@
 pub const VIN_DEFAULT: &str = "(?i)\\b[A-HJ-NPR-Z0-9]{17}\\b";
 pub const LPN_LTU: &str = r"\b[A-Z]{3}[-\s:]?\d{3}\b";
+pub const LPN_FIN: &str = r"\b[A-Z]{3}[-\s:]?\d{3}\b";
+pub const LPN_HUN: &str = r"\b[A-Z]{3}[-\s:]?\d{3}\b";
+pub const LPN_FRA: &str = r"\b[A-Z]{2}[-\s:]?\d{2}[-\s:]?[A-Z]{2}\b";
+pub const LPN_ITA: &str = r"\b[A-Z]{2}[-\s:]?\d{3}[-\s:]?[A-Z]{2}\b";
