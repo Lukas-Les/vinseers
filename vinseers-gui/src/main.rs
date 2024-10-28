915e51543f9fd8d29ea76b8a035c2f3758e05e03
stabmeldys <lukas.les93@gmail.com>
2024-10-28 17:44:27 +0200

    theme picker implemented

--- vinseers-gui/src/main.rs
+++ vinseers-gui/src/main.rs
@@ -5,7 +5,7 @@ use std::string::ToString;
 use rfd::FileDialog;
 
 use iced::widget::text_editor::Content;
-use iced::widget::{column, row, text_editor, Button, PickList};
+use iced::widget::{column, horizontal_space, row, text_editor, Button, PickList};
 use iced::{Element, Theme};
 
 use vinseers::{helpers, regexes, vid::VidType, vid::LpnType};
@@ -17,12 +17,13 @@ pub fn main() -> iced::Result {
 }
 
 fn theme(state: &State) -> Theme {
-    Theme::Dark
+    state.theme.clone()
 }
 
 struct State {
     content: text_editor::Content,
     vid: VidType,
+    theme: Theme,
 }
 
 impl Default for State {
@@ -30,6 +31,7 @@ impl Default for State {
         Self {
             content: text_editor::Content::with_text(constants::RESULT_TEXT_DEFAULT),
             vid: VidType::Vin,
+            theme: Theme::Dark,
         }
     }
 }
@@ -62,6 +64,9 @@ fn update(state: &mut State, message: Message) {
             state.vid = vid;
             println!("vid regex: {}", &state.vid.to_regex());
         }
+        Message::SelectTheme(theme) => {
+            state.theme = theme;
+        }
     }
 }
 
@@ -72,11 +77,19 @@ fn view(state: &State) -> Element<Message> {
         Message::VidSelected,
     );
 
+    let theme_pick_list = PickList::new(
+        Theme::ALL.as_ref(),
+        Some(&state.theme),
+        Message::SelectTheme,
+    );
+
     let top_row = row![
         Button::new("Select Files").on_press(Message::SelectFiles),
         Button::new("Select directory").on_press(Message::SelectDir),
         Button::new("Reset").on_press(Message::ResetResult),
         re_pick_list,
+        horizontal_space(),
+        theme_pick_list,
     ]
     .spacing(3);
     let result_display = text_editor(&state.content).on_action(Message::AnyActionPerformed);
@@ -91,4 +104,5 @@ enum Message {
     ResetResult,
     VidSelected(VidType),
     AnyActionPerformed(text_editor::Action),
+    SelectTheme(Theme),
 }
